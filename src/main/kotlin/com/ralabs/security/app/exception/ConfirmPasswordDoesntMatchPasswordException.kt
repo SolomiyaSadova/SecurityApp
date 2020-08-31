bbe387f886959100e434c5a217587c661df3ead4
SolomiyaSadova <ssolimia@gmail.com>
2020-08-31 16:00:41 +0300

    Add refresh token

---
+++ src/main/kotlin/com/ralabs/security/app/exception/ConfirmPasswordDoesntMatchPasswordException.kt
@@ -0,0 +1,3 @@
+package com.ralabs.security.app.exception
+
+class ConfirmPasswordDoesntMatchPassword(override val message: String?) : Exception(message)
---
+++ src/main/kotlin/com/ralabs/security/app/exception/UserAlreadyExistsException.kt
@@ -0,0 +1,4 @@
+package com.ralabs.security.app.exception
+
+class UserAlreadyExistsException {
+}
